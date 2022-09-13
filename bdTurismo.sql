0ddbe1c75f6bba702c383bf1564966c0e18bb8c0
Marcos Matioli <matiolimarcos@gmail.com>
2022-09-13 02:01:01 -0300

    Foi inserido 6 dados 2 em cada tabela como solicitado

--- bdTurismo.sql
+++ bdTurismo.sql
@@ -1,12 +1,5 @@
-/*banco -oreon
-
-fazer 6 inserts
-fazer 3 updates
-fazer um inner join juntando as 3 tabelas
-fazer 4 commits
-*/
 create database bdTurismo;
-use bdTurimos;
+use bdTurismo;
 /*tabelas- cliente(cod cli,nome, idade, sexo, email, telefone,*/
 create table tbCliente(
 cod_cli int primary key AUTO_INCREMENT,
@@ -17,10 +10,10 @@ email varchar(50),
 telefone varchar(15)
 );
 /*planos(cod plano,tipo, descricação)*/
-create table tbPlano(
+create table tbPlanos(
 cod_plano int primary key AUTO_INCREMENT,
-tipo varchar(50),
-descricao varchar(100)
+tipo varchar(200),
+descricao varchar(200)
 );
 
 /*pedido(cod pedido,cod cli,cod plano, data,hora,valor,descricao.*/
@@ -31,7 +24,22 @@ cod_plano int,
 data varchar(20),
 hora varchar(20),
 valor DECIMAL(7,2),
-descricao varchar(100),
-foreign key (cod_cli) references Cliente(cod_cli),
-foreign key (cod_plano) references tbPlano(cod_plano)
+descricao varchar(200),
+foreign key (cod_cli) references tbCliente(cod_cli),
+foreign key (cod_plano) references tbPlanos(cod_plano)
 );
+
+/*banco -oreon
+fazer 6 inserts
+fazer 3 updates
+fazer um inner join juntando as 3 tabelas
+fazer 4 commits
+*/
+insert into tbCliente (nome_cli,idade,sexo,email,telefone) values ("Batman",35,"M","batnabatcaverna@heroi.com.br","966668987");
+insert into tbCliente (nome_cli,idade,sexo,email,telefone) values ("Mulher Gato",18,"F","mulhergatomiau@vila.com.br","987679687");
+
+insert into tbPlanos (tipo,descricao) values ("Plano Tour New York", "Plano de viagem pelos Estados Unidos, conheça NY, Texas, Canada e Havai");
+insert into tbPlanos (tipo,descricao) values ("Plano Tour Italia", "Plano Italia, conheça a Torre de Pisa, Vaticano e comer a comida local que é muito 'MASSA'!!");
+
+insert into tbPedido (data,hora,valor,descricao) values ("20-09-2022","20:20",10567.90,"Comprado no dia 19-09 e foi parcelado a pessoa está emitindo o visto para prosseguir com o processo");
+insert into tbPedido (data,hora,valor,descricao) values ("19-11-2023","13:20",20567.90,"Pct Italia, o cliente solicitou uma passada em vários pontos turisticos o que deixou muito mais caro");
